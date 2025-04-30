2353d4780d539d11a20b2c78eb0423b3099a3c8d
Tungsten-Vet <thanhtclone01@gmail.com>
2025-04-30 17:29:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ commands['verify'] = {
         }
 
         const name = args.slice(1).join(' ');
-        const webhookURL = 'https://your-webhook-url.com'; // <-- sửa lại
+        const webhookURL = 'c5tntlbji165dbkyxqe92b0jv8xsi16j@hook.eu2.make.com'; // <-- sửa lại
 
         axios.post(webhookURL, { name, discord: message.author.username })
             .then(() => {
